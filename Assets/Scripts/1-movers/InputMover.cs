da3631b9ef529fc83b0ec218470c7e77a9a9573a
coeffic-raphael <156908784+coeffic-raphael@users.noreply.github.com>
2025-11-17 16:44:11 +0200

    Update InputMover.cs

--- Assets/Scripts/1-movers/InputMover.cs
+++ Assets/Scripts/1-movers/InputMover.cs
@@ -7,14 +7,11 @@ using UnityEngine.InputSystem;
 public class InputMover: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 10f;
-
     [SerializeField] InputAction move = new InputAction(
         type: InputActionType.Value, expectedControlType: nameof(Vector2));
-
     void OnEnable()  {
         move.Enable();
     }
-
     void OnDisable()  {
         move.Disable();
     }
@@ -22,12 +19,6 @@ public class InputMover: MonoBehaviour {
     void Update() {
         Vector2 moveDirection = move.ReadValue<Vector2>();
         Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
-        transform.position += movementVector;
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
-        //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
-        // It makes a difference only if the object is rotated.
+        transform.position += movementVector;.
     }
 }
